2ea226a6fa1e369e33cadf10b90c385f256e505e
mustafamertcakar <mustafacakarr1@gmail.com>
2024-09-28 14:33:13 +0300

    3 Panelli Mvc Kutuphane Yonetim Sistemi
    Udemy Murat Yucedag kursunu izleyip yaptıgım proje.
    ASP.NET MVC5 ile geliştirilmiştir.

---
+++ MvcKutuphane/MvcKutuphane/Controllers/AyarlarController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+namespace MvcKutuphane.Controllers
+{
+    public class AyarlarController : Controller
+    {
+        // GET: Ayarlar
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var kullanicilar = db.TBLADMIN.ToList();
+            return View(kullanicilar);
+        }
+        public ActionResult Index2()
+        {
+            var kullanicilar = db.TBLADMIN.ToList();
+            return View(kullanicilar);
+        }
+        [HttpGet]
+        public ActionResult YeniAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniAdmin(TBLADMIN t)
+        {
+            db.TBLADMIN.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Index2");
+        }
+        public ActionResult AdminSil(int id)
+        {
+            var admin = db.TBLADMIN.Find(id);
+            db.TBLADMIN.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index2");  
+        }
+        [HttpGet]
+        public ActionResult AdminGuncelle(int id)
+        {
+            var admin = db.TBLADMIN.Find(id);
+            return View("AdminGuncelle", admin);
+        }
+        [HttpPost]
+        public ActionResult AdminGuncelle(TBLADMIN t)
+        {
+            var admin = db.TBLADMIN.Find(t.ID);
+            admin.Kullanici = t.Kullanici;
+            admin.Sifre = t.Sifre;
+            db.SaveChanges();
+            return RedirectToAction("Index2");
+        }
+    }
+}
---
+++ MvcKutuphane/MvcKutuphane/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MvcKutuphane.Models.Entity;
+namespace MvcKutuphane.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        // GET: Login
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GirisYap(TBLUYELER p)
+        {
+            var bilgiler = db.TBLUYELER.FirstOrDefault(x =>x.MAIL == p.MAIL && x.SIFRE == p.SIFRE);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.MAIL, false);
+                Session["Mail"] = bilgiler.MAIL.ToString();
+                Session["AdminPhoto"] = bilgiler.FOTOGRAF;
+                Session["AdminName"] = bilgiler.AD;
+                Session["AdminSurname"] = bilgiler.SOYAD;
+                return RedirectToAction("Index", "Panelim");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
